f2cd822755e973c59176014b62c9884c6262e233
jordanparnold222 <42490695+jordanparnold222@users.noreply.github.com>
2021-11-14 13:15:23 -0700

    Add project files.

---
+++ FavoriteMusicVideo/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+var opacity = 0;
+
+function videoAppear() {
+    if (opacity < 1) {
+        opacity += .1;
+        setTimeout(function () { videoAppear() }, 100);
+    }
+    document.getElementById('Video_Container').style.opacity = opacity;
+}
